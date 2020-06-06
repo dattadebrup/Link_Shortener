cbbb222e7d2d641a437f4d4ff8323ae98d8604a6
Unknown <dattadebrup@gmail.com>
2020-06-06 23:38:13 +0530

    Web pages added.

--- shorten/__init__.py
+++ shorten/__init__.py
@@ -1,24 +1,46 @@
 import logging
 
 import azure.functions as func
+from github import Github
+import random
+import string
+import requests
+import time
 
+f = open("shorten/git_token.txt", "r")
+token = str(f.read())
+g = Github(token)
+repo = g.get_repo("dattadebrup/link")
+headers = {"Access-Control-Allow-Origin": "*"}
+
+def get_random_alphaNumeric_string(stringLength=5):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
+    
+    url = req.params.get('url')
+    if not url:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            url = req_body.get('url')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    if url:
+        ran_string= get_random_alphaNumeric_string()
+        #content = '<head> <meta http-equiv="refresh" content="0; URL='+url+'" /> </head> <body> <p>If you are not redirected, <a href="'+url+'">click here</a>.</p> </body>'
+        content = url 
+        repo.create_file("/sites/"+ran_string + ".txt" , "New link added", content, branch="master")
+
+        myobj = {'url': 'https://dattadebrup.github.io/Link_Shortener/shorten.html?li=' + ran_string}
+        short_link = "https://git.io/" + requests.post('https://git.io/create', data = myobj).text 
+        #time.sleep(0)
+        return func.HttpResponse(short_link, headers = headers)
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+             "Please pass a url on the query string or in the request body",
+             status_code=400, headers = headers
         )
